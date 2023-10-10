c02b9c92f33b1562ecb7acff09cb8f9437a2e9b4
kasunc.zincat <kasunc.zincat@gmail.com>
2023-10-10 15:43:47 +0530

    fix the node backend sql issues

--- backend/server.cjs
+++ backend/server.cjs
@@ -27,12 +27,12 @@ con.connect((err) => {
 
 // Define a route to handle POST requests for updating employees by ID
 app.post("/api/update-data/:id", (req, res) => {
-  const mainFormData = req.body; // This will contain the main form data
-  const tableData = mainFormData.tableData;
+  const mainFormData = req.body.mainFormData; // This will contain the main form data
+  const tableData = req.body.tableData;
   // Extract the 'id' parameter from the URL
   const id = req.params.id; // Access 'id' from URL parameter
   update_employees(res, mainFormData, tableData, id);
-  return res.status(200).json({ message: "Employee updated successfully" });
+  // return res.status(200).json({ message: "Employee updated successfully" });
 });
 
 // Start the Express.js server
@@ -41,7 +41,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-function update_employees(res, mainFormData, tableData, id) {
+function update_employees(res, mainFormData, tableData, employee_id) {
   // Extract data from the request body
   const {
     customer_type,
@@ -59,24 +59,8 @@ function update_employees(res, mainFormData, tableData, id) {
   } = mainFormData;
 
   // SQL statement to update an employee by 'id'
-  const sql = `
-    UPDATE employees
-    SET
-      customer_type = ?,
-      Refference_number = ?,
-      title = ?,
-      company_name = ?,
-      customer_name = ?,
-      identity_number = ?,
-      opening_balance = ?,
-      sales_person = ?,
-      posting_address = ?,
-      billing_address = ?,
-      location_1 = ?,
-      location_2 = ?
-    WHERE
-      employee_id = ?
-  `;
+  const sql =
+    "UPDATE `react_h2bis_sample`.`employees` SET `customer_type`=?, `reff_no`=?, `title`= ?, `company_name`= ?, `cust_name`= ?, `identity_number`= ?, `open_balance`= ?, `sales_person`= ?, `posting_address`= ?, `billing_address`= ?, `location1`= ?, `location2`= ? WHERE  `employee_id`= ?";
 
   const values = [
     customer_type,
@@ -91,34 +75,28 @@ function update_employees(res, mainFormData, tableData, id) {
     billing_address,
     location1,
     location2,
-    id, // 'id' as the last parameter
+    employee_id,
   ];
 
   // Execute the SQL statement
-  let empid = con.query(sql, values, (err, result) => {
+  con.query(sql, values, (err, result) => {
     if (err) {
       // Handle the error
-      return res.status(500).json({ error: "Error updating extra employee" });
-    } else {
-      // Check if the result object has an insertId property
-      if (result && result.insertId) {
-        const insertedId = result.insertId;
-        console.log(`Inserted row ID: ${insertedId}`);
-        // Now you can use 'insertedId' as needed
-        return insertedId;
-      } else {
-        console.log("No row was inserted or the insertId is not available.");
-      }
+      return res.status(500).json({ error: "Error updating employee" });
+    }
+    if (result.affectedRows === 0) {
+      // No rows were updated, so the employee with 'id' was not found
+      return res.status(404).json({ message: "Employee not found" });
     }
   });
 
-  updateTableRecords(res, tableData, empid);
+  updateTableRecords(res, tableData, employee_id);
 }
 
-function updateTableRecords(res, tableData, emp_id) {
+function updateTableRecords(res, tableData, employee_id) {
   tableData.forEach((element) => {
     const { name, designation, mobile_no, email, address } = element;
-    const values = [1, name, designation, mobile_no, email, address];
+    const values = [employee_id, name, designation, mobile_no, email, address];
 
     const sql = `
 INSERT INTO extra_employee_details (employee_id, name, designation, mobile, email, address)
@@ -129,7 +107,6 @@ VALUES (?, ?, ?, ?, ?, ?)`;
         console.error("Error executing SQL:", err);
         return res.status(500).json({ error: "Error updating employee" });
       }
-      // Handle the result
     });
   });
 }
